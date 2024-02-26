3aea0ea00896f3f72e0b4e14b0cf03a07bb29f79
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-02-26 11:13:47 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace ProGitForProgrammers
+{
+ class Program
+   {
+ static void Main(string[] args)
+ {
+    Console.WriteLine("Hello World!");
+    Console.WriteLine("I just added this in Visual Studio");
+ }
+ }
+}
