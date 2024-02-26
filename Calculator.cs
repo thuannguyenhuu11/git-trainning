6e0d908343356620d5280b2fe96aa2af8dc51890
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-02-26 14:10:27 +0700

    Add Multiply and Divide method

--- Calculator.cs
+++ Calculator.cs
@@ -7,3 +7,12 @@ public int Add (int left, int right)
  {
  return left - right;
  }
+
+ public int Multiply(int left, int right)
+ {
+ return left * right;
+ }
+public int Divide(int left, int right)
+ {
+ return left / right;
+ }
